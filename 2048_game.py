913c3a03ebae1a0be127b9d653290e8fd495ec5c
Prayush <prayushbogati0120@gmail.com>
2026-01-21 18:17:56 +0545

    '2048' game commit

---
+++ 2048_game.py
@@ -0,0 +1,139 @@
+import random
+
+class Game:
+    def __init__(self):
+        self.grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+        for _ in range(2):
+            self.add_random_tile()
+
+    def display_grid(self):
+        for row in self.grid:
+            print(" ".join(str(num) for num in row))
+
+    def add_random_tile(self):
+        while True:
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+            if self.grid[row][col] == 0:
+                self.grid[row][col] = random.choice([2, 4])
+                break
+        return self.grid
+
+    def move_left(self):
+        for i in range(4):
+            self.grid[i] = [num for num in self.grid[i] if num!= 0]
+
+            j = 0
+            while j < len(self.grid[i]) - 1:
+                if self.grid[i][j] == self.grid[i][j+1]:
+                    self.grid[i][j] *= 2
+                    self.grid[i][j + 1] = 0
+                    j+= 2
+                else:
+                    j+= 1
+            
+            self.grid[i] = [num for num in self.grid[i] if num!= 0]
+
+            self.grid[i] += [0] * (4 - len(self.grid[i]))
+
+    def move_right(self):
+        for i in range(4):
+            self.grid[i].reverse()   #flip
+            self.grid[i] = [num for num in self.grid[i] if num!= 0]
+
+            j = 0
+            while j < len(self.grid[i]) - 1:
+                if self.grid[i][j] == self.grid[i][j+1]:
+                    self.grid[i][j] *= 2
+                    self.grid[i][j + 1] = 0
+                    j+= 2
+                else:
+                    j+= 1
+            
+            self.grid[i] = [num for num in self.grid[i] if num!= 0]
+
+            self.grid[i] += [0] * (4 - len(self.grid[i]))
+            self.grid[i].reverse()               #flip
+
+    def transpose(self):
+        transposed = []
+        for col in range(len(self.grid)):
+            new_row = []
+            for row in range(len(self.grid)):
+                new_row.append(self.grid[row][col])
+            transposed.append(new_row)
+        self.grid = transposed
+    
+    def move_up(self):
+        self.transpose()
+        self.move_left()
+        self.transpose()
+
+    def move_down(self):
+        self.transpose()
+        self.move_right()
+        self.transpose()
+
+    def is_game_won(self):
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid)):
+                if self.grid[row][col] == 2048:
+                    return True
+        return False
+
+    def is_game_over(self):
+        for row in self.grid:
+            if 0 in row:
+                return False
+
+        for i in range(4):
+            for j in range(3):
+                if self.grid[i][j] == self.grid[i][j + 1]:
+                    return False
+                    
+        for i in range(3):
+            for j in range(4):
+                if self.grid[i][j] == self.grid[i + 1][j]:
+                    return False
+
+        return True
+
+    def run(self):
+        while True:
+            self.display_grid()
+
+            move = input("Enter move (w/a/s/d) or q to quit: ").lower()
+
+            temp_grid = self.grid.copy()
+            
+            if move == "q":
+                print("Thanks for playing!")
+                break
+            elif move == "a":
+                self.move_left()
+                
+            elif move == "d":
+                self.move_right()
+
+            elif move == "w":
+                self.move_up()
+
+            elif move == "s":
+                self.move_down()
+
+            else:
+                print("Invalid input!")
+
+            if temp_grid != self.grid:
+                    self.add_random_tile()
+            if self.is_game_won():
+                print("Congratulations! You've reached 2048")
+            if self.is_game_over():
+                print("Game over! No moves left")
+                break
+        
+
+
+game = Game()  
+game.run()
