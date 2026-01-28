1d7d9377f407ff4246f8208f51c8f4e9f1724ff5
Prayush Bogati <127077497+prayushbogati@users.noreply.github.com>
2026-01-28 08:30:16 +0000

    adding new file numpy.py

---
+++ numpy.py
@@ -0,0 +1,12 @@
+# MATRICES
+
+# unlike arrays, matrices can't have more than 2 dimensions
+
+# create matrices using np.array() or np.matrix()[not recommended though]
+# matrices are special as they can have  * and ** operations between them
+
+# using arrays there are methods like np.dot for matrix mul as np.matrix is not suggested 
+
+# transposition using np.transpose()
+
+# create identity matrix using np.eye(n) or np.identity(n)
